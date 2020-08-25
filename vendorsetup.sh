6911c9d7c01699d4142bd5ad3cd5cadeb368134c
modit jain <moditjain121@gmail.com>
2020-08-25 15:05:09 +0000

    vendorsetup added

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+rm -rf vendor/pixelgapps/priv-app/SetupWizardPrebuilt/SetupWizardPrebuilt.apk
+git clone https://github.com/moditjain9777/Setupwizard.git vendor/pixelgapps/priv-app/SetupWizardPrebuilt
