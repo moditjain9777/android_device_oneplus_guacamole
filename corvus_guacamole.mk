5abd00dbac53188f5aa282fec7ebf72e314ceb21
Kingsman44 <shivan.0972@gmail.com>
2020-09-18 04:01:03 +0000

    mido: add gapps support for R
    gapps for r :https://gitlab.com/Kingsman-z/gapps
    
    pixelstyle: https://github.com/Kingsman44/vendor_pixelstyle-R

--- corvus_guacamole.mk
+++ corvus_guacamole.mk
@@ -12,7 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/corvus/config/common.mk)
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/gapps/config.mk)
+$(call inherit-product, vendor/pixelstyle/config.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := corvus_guacamole
@@ -37,3 +40,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+# Google Recorder
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
